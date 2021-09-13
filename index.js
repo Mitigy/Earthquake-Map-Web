6af57ad0c0e79938a5f436239223daaa2ad1e4ba
Moocrafter <38020594+Moocrafter@users.noreply.github.com>
2021-09-13 07:51:29 -0400

    Disabled Street View Use (V1.1)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 0, lng: 0 },
         zoom: 1.5,
+        streetViewControl: false,
     });
 
     (async function() {
